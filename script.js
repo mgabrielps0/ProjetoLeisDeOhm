b81309cf7a7de5df8dc7537fb001b5fa258a8e67
mgabrielps0 <136991980+mgabrielps0@users.noreply.github.com>
2025-04-06 22:40:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function loadContent(section) {
     const content = document.getElementById("content");
-
-    // Definimos o caminho do arquivo HTML correspondente
+    
     const url = `secoes/${section}.html`;
 
-    // Usamos fetch() para carregar o conteúdo do arquivo HTML
     fetch(url)
         .then(response => {
             if (!response.ok) {
@@ -13,8 +11,8 @@ function loadContent(section) {
             return response.text();
         })
         .then(html => {
-            content.innerHTML = html; // Insere o conteúdo carregado na página
-            content.className = ""; // Remove classes anteriores
-            content.classList.add("content", section); // Adiciona classe com o nome da seção
+            content.innerHTML = html;
+            content.className = "";
+            content.classList.add("content", section);
         })
 }
